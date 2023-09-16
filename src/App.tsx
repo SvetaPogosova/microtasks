eb629f208c88624b9b811b134e15dbe3263802b5
SvetaPogosova <svet.pogosova@gmail.com>
2023-09-16 22:58:39 +0300

    associative array

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,85 @@
 import React, {useState} from 'react';
 import './App.css';
-import {FullInput} from "./components/Fullinput";
-import {Input} from "./components/Input";
-import {Button} from "./components/Button";
+import {Todolist} from './Todolist';
+
+export type todolistsType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
-    const [message, setMessage] = useState([
-            {message: 'message1'},
-            {message: 'message2'},
-            {message: 'message3'},
-            {message: 'message4'},
-            {message: 'message5'}
+
+    let todolistID1 = crypto.randomUUID();
+    let todolistID2 = crypto.randomUUID();
+
+    let [todolists, setTodolists] = useState<Array<todolistsType>>([
+        {id: todolistID1, title: 'What to learn', filter: 'all'},
+        {id: todolistID2, title: 'What to buy', filter: 'all'},
+    ])
+
+    let [tasks, setTasks] = useState({
+        [todolistID1]: [
+            {id: crypto.randomUUID(), title: "HTML&CSS", isDone: true},
+            {id: crypto.randomUUID(), title: "JS", isDone: true},
+            {id: crypto.randomUUID(), title: "ReactJS", isDone: false},
+            {id: crypto.randomUUID(), title: "Rest API", isDone: false},
+            {id: crypto.randomUUID(), title: "GraphQL", isDone: false},
+        ],
+        [todolistID2]: [
+            {id: crypto.randomUUID(), title: "HTML&CSS2", isDone: true},
+            {id: crypto.randomUUID(), title: "JS2", isDone: true},
+            {id: crypto.randomUUID(), title: "ReactJS2", isDone: false},
+            {id: crypto.randomUUID(), title: "Rest API2", isDone: false},
+            {id: crypto.randomUUID(), title: "GraphQL2", isDone: false},
         ]
-    )
-    let [title, setTitle] = useState('')
-    const addMessage = (title: string) => {
-        const newMessage = {message: title}
-        setMessage([newMessage, ...message])
+    });
+
+    function removeTask(todolistID: string, id: string) {
+        setTasks({...tasks, [todolistID]: tasks[todolistID].filter(t => t.id !== id)})
     }
-    const callBackBtnHandler = () => {
-        addMessage(title)
-        setTitle('')
+
+    function addTask(todolistID: string, title: string) {
+        setTasks({
+            ...tasks,
+            [todolistID]: [{id: crypto.randomUUID(), title: title, isDone: false}, ...tasks[todolistID]]
+        })
     }
+
+    function changeStatus(todolistID: string, taskId: string, isDone: boolean) {
+        setTasks({
+            ...tasks,
+            [todolistID]: tasks[todolistID].map(el => el.id === taskId ? {...el, isDone} : el)
+        });
+    }
+
+    function changeFilter(todolistID: string, value: FilterValuesType) {
+        setTodolists(todolists.map(el => el.id === todolistID ? {...el, filter: value} : el))
+    }
+
+
     return (
         <div className="App">
-            {/*<FullInput addMessage={addMessage}/>*/}
-            <Input title={title} setTitle={setTitle}/>
-            <Button name={'BUTTON'} callBack={(callBackBtnHandler)}/>
-
-            {message.map((el, index) => {
+            {todolists.map(el => {
+                let tasksForTodolist = tasks[el.id];
+                if (el.filter === "active") {
+                    tasksForTodolist = tasks[el.id].filter(t => !t.isDone);
+                }
+                if (el.filter === "completed") {
+                    tasksForTodolist = tasks[el.id].filter(t => t.isDone);
+                }
                 return (
-                    <div key={index}>{el.message}</div>
+                    <Todolist key={el.id}
+                              todolistID={el.id}
+                              title={el.title}
+                              tasks={tasksForTodolist}
+                              removeTask={removeTask}
+                              changeFilter={changeFilter}
+                              addTask={addTask}
+                              changeTaskStatus={changeStatus}
+                              filter={el.filter}
+                    />
                 )
             })}
         </div>
